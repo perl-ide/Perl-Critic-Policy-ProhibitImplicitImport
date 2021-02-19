d760b085b3227b061251eeed094ecc3a86530466
Olaf Alders <olaf@wundersolutions.com>
2021-02-19 17:57:20 -0500

    increment $VERSION after 0.000001 release

--- lib/Perl/Critic/Policy/ProhibitImplicitImport.pm
+++ lib/Perl/Critic/Policy/ProhibitImplicitImport.pm
@@ -3,7 +3,7 @@ package Perl::Critic::Policy::ProhibitImplicitImport;
 use strict;
 use warnings;
 
-our $VERSION = '0.000001';
+our $VERSION = '0.000002';
 
 use Perl::Critic::Utils qw($SEVERITY_LOW);
 use parent 'Perl::Critic::Policy';
